0fe2c10216d2483dd97700774e3edcfb7ece084e
Ta-SeenJunaid <taseen.junaid@gmail.com>
2020-07-12 10:14:06 +0600

    race.go

---
+++ golang/chebyrash_promise/race.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/chebyrash/promise"
+)
+
+func main() {
+	var p1 = promise.Resolve("Promise 1")
+	var p2 = promise.Resolve("Promise 2")
+
+	fastestResult, _ := promise.Race(p1, p2).Await()
+
+	fmt.Printf("Both resolve, but %s is faster", fastestResult)
+}
